8403037fcc4743794e31f8cf07c8e7e84c9eb0c7
muslimkhan44 <102409260+muslimkhan44@users.noreply.github.com>
2022-04-05 15:12:49 +0500

    Add files via upload

---
+++ Untitled11.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# In[2]:
+
+names=("Ali","abdan","hasan","hamza")
+print(names)
+
+
+# In[3]:
+
+print(names[0])
+
+
+# In[4]:
+
+print(names[1])
+
+
+# In[5]:
+
+print(names[2])
+
+
+# In[7]:
+
+print(names[3])
+
+
+# In[ ]:
+
+
+
